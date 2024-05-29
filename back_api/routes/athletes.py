a3657780585033ffd78d1da73baea23ca586a37c
AKO_Berenger <vlunaski@gmail.com>
2024-05-29 11:18:48 +0200

    ✨(api) build results endpoints

--- back_api/routes/athletes.py
+++ back_api/routes/athletes.py
@@ -1,9 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from fastapi.responses import JSONResponse
-from services.athletes import f_athletes, get_all, get_athlete_id, get_athlete_name
-
-
-
+from services.athletes import f_athletes, get_all, get_athlete_id, get_athlete_name, top_10_athletes_by_year
 
 athletes = APIRouter()
 
@@ -43,6 +40,14 @@ async def get_athlete_by_name(name: str):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+"""Get top 10 athletes by year"""
+@athletes.get("/top_athletes_by_year/{game_year}")
+async def get_top_athletes_by_year(game_year: int):
+    athletes_list = await top_10_athletes_by_year(game_year)
+    try:
+        return JSONResponse(content=athletes_list)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
     
