be8d24039cd355abe94f4ed23b3f93148ecc0b5d
Matias gomez <mrmatiasgomez@gmail.com>
2024-05-29 23:25:45 +0200

    Fix PredictionPage

--- front/jo-ia-ipssi/src/App.js
+++ front/jo-ia-ipssi/src/App.js
@@ -3,7 +3,6 @@ import Homepage from "./pages/Homepage";
 import Data from "./pages/Data";
 import AboutPage from "./pages/AboutPage";
 import PositionTorch from "./pages/PositionTorch";
-import PredictionPage from "./pages/PredictionPage";
 import FaqPage from "./pages/FaqPage";
 
 function App() {
@@ -14,9 +13,7 @@ function App() {
         <Route path="/olympic-results" element={<Data />} />
         <Route path="/a-propos" element={<AboutPage />} />
         <Route path="/position-flamme" element={<PositionTorch />} />
-        <Route path="/prediction" element={<PredictionPage />} />
         <Route path="/faq" element={<FaqPage />} />
-        <Route path="/prediction" element={<PredictionPage />} />
       </Routes>
     </BrowserRouter>
   );
