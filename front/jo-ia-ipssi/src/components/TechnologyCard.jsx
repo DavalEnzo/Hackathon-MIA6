e50252670f0a7c3f32e0bdbaadf88d21ac194652
Matias gomez <mrmatiasgomez@gmail.com>
2024-05-30 15:03:28 +0200

    add Hosts card & more techno

--- front/jo-ia-ipssi/src/components/TechnologyCard.jsx
+++ front/jo-ia-ipssi/src/components/TechnologyCard.jsx
@@ -3,7 +3,7 @@ import { Card } from 'react-bootstrap';
 
 const TechnologyCard = ({ technology }) => {
   return (
-    <Card className='border-0'>
+    <Card className='border-0 mt-4'>
       <Card.Img className='w-50 m-auto' variant="top" src={technology.logo} />
       <Card.Body className='text-center'>
         <Card.Title>{technology.name}</Card.Title>
